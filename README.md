8e567148c477a6c8812e730d2b311a6d9f1d385c
xxcvz <newaccfridge@op.pl>
2023-12-05 02:30:09 +0100

    link fix
    removes discord invites from messages

---
+++ README.md
@@ -0,0 +1,11 @@
+# catbot-logbot
+Created for relaying Team Fortress chat to discord chat
+##### Instructions:
+1. Download Node.JS (latest version)
+2. Git clone this repo
+3. Make private.json by using private.example.json
+4. Run "npm install" to install all required packages as well as pm2
+5. Run the script using command "pm2 start logbot1.js"
+##### Changes:
+Removes "discord.gg/" from messages to disable invite spam in relay channel.
+To ensure no embed links are sent, go to channel settings and disable "Embed Links" for the discord bot.
---
+++ logbot1.js
@@ -0,0 +1,218 @@
+/*
+	npm install dateformat discord.js tail
+*/
+
+const { token } = require('./private.json');
+
+const Tail = require('tail').Tail;
+const Discord = require('discord.js');
+const dateformat = require('dateformat');
+const fs = require('fs');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(token);
+
+
+
+
+var queue = [];
+var stack = [];
+
+const stack_size = 10;
+var stack_iterator = 0;
+
+function splitCSV(csv) {
+	let result = [];
+	let current = '';
+	let quotes = false;
+	for (let i = 0; i < csv.length; ++i) {
+		if (csv[i] == '"' && csv[i + 1] == '"') {
+			current += '"';
+			++i;
+			continue;
+		}
+		if (csv[i] == '"') {
+			quotes = !quotes;
+			continue;
+		}
+		if (csv[i] == ',' && !quotes) {
+			result.push(current);
+			current = '';
+			continue;
+		}
+		current += csv[i];
+	}
+	result.push(current);
+	return result;
+}
+
+let antiSpamData = {};
+let muted = {};
+
+function antiSpam(data) {
+	let steamID = data[1];
+	if (muted[steamID])
+		return false;
+	if (!antiSpamData[steamID])
+		antiSpamData[steamID] = { count: 0, last: 0 };
+	if (Date.now() - antiSpamData[steamID].last > 15 * 1000)
+		antiSpamData[steamID].count = 0;
+	antiSpamData[steamID].count++;
+	antiSpamData[steamID].last = Date.now();
+	if (antiSpamData[steamID].count > 8) {
+		//console.log(`Banning ${steamID} for spamming`);
+		antiSpamData[steamID].banned = Date.now();
+	}
+	return !antiSpamData[steamID].banned;
+}
+
+function composeMessage(data) {
+	let steamID = data[1];
+	let username = data[2];
+	let message = data[3];
+	let ipcID = data[4];
+
+
+    const wordToReplace = 'discord.gg/';  
+    const replacementWord = '<removed>';  
+
+    message = message.replace(new RegExp(wordToReplace, 'gi'), replacementWord);
+
+    return `\`[ID ${ipcID}] [U:1:${steamID}]\` **${username}:** ${message}`;
+}
+
+function composeMessageRaw(data) {
+	let steamID = data[1];
+	let username = data[2];
+	let message = data[3];
+	let ipcID = data[4];
+
+	return `[ID ${ipcID}] [U:1:${steamID}] ${username}: ${message}`;
+}
+
+function test_and_set(msg) {
+	var j = stack_iterator;
+	for (var i = 0; i < 10; i++) {
+		if (stack[j] == msg) return false;
+		j++;
+		if (j >= stack_size) j = 0;
+	}
+	stack[stack_iterator++] = msg;
+	if (stack_iterator >= stack_size) stack_iterator = 0;
+	return true;
+}
+
+function onLine(data) {
+	try {
+		if (test_and_set(data)) {
+			queue.push(data);
+		}
+	} catch (e) { }
+}
+
+client.on('message', (msg) => {
+	try {
+		if (!msg.member.hasPermission('MANAGE_GUILD')) return;
+		if (msg.content.indexOf('$$mute') == 0) {
+			var id = /(\d+)/.exec(msg.content)[0];
+			if (muted[id]) {
+				msg.channel.send(`Unmuting [U:1:${id}]`);
+				console.log('unmuting', id);
+				muted[id] = false;
+			}
+			else {
+				msg.channel.send(`Muting [U:1:${id}]`);
+				console.log('muting', id);
+				muted[id] = true;
+			}
+		}
+	}
+	catch (e) { }
+});
+
+function send() {
+	try {
+		let msg = '​​';
+		let msgRaw = '';
+		if (!queue.length) return;
+		while (queue.length) {
+			try {
+				let csv = queue.shift();
+				let data = splitCSV(csv);
+				if (!antiSpam(data))
+					continue;
+				// Add more bot names as desired to filter out
+				if (data[2].match(/(just disable vac tf|cat-bot) \d+$/))
+					continue;
+				let message = composeMessage(data);
+
+				msg += message + '\n'
+				msgRaw += composeMessageRaw(data) + '\n';
+			}
+			catch (e) {
+				console.log('error', e);
+			}
+		}
+		if (msgRaw == '') return;
+		try {
+			process.stdout.write(msgRaw);
+			let chans = client.channels.cache.filter(channel => channel.type === 'text').filter(channel => channel.name === 'tf2-chat-relay').array();
+			for (let channel of chans) {
+				channel.send(msg);
+			}
+		}
+		catch (e) { }
+	} catch (e) { }
+}
+
+setInterval(send, 8000);
+
+function onError(error) {
+	console.log('ERROR:', error);
+}
+
+let watching = {};
+var tails = [];
+
+function locateLogs() {
+	try {
+		fs.readdir('/opt/cathook/data', (error, files) => {
+			if (error) {
+				console.log(error);
+				return;
+			}
+			for (let file of files) {
+				file = '/opt/cathook/data/' + file;
+				if (!watching[file] && /chat-.+\.csv/.exec(file)) {
+					console.log(`Found log file: ${file}`);
+					let tail = new Tail(file);
+					tail.on('line', onLine);
+					tail.on('error', onError);
+					tails.push(tail);
+					watching[file] = true;
+				}
+			}
+		});
+	} catch (e) { }
+}
+
+client.on('ready', () => {
+	client.user.setActivity('tf2 relay', { type: "PLAYING" });
+	client.guilds.cache.forEach((guild, str, map) => {
+		var has_channel = guild.channels.cache.filter(channel => channel.type === 'text').filter(channel => channel.name === 'tf2-chat-relay').array().length;
+		if (!has_channel)
+			guild.channels.create("tf2-chat-relay", { reason: 'Need somewhere to relay the messages'}).then((channel) => {
+				console.log("Created relay channel!");
+				channel.send("This channel will relay the chat of all bots.\n\nUse $$mute (steamid32) in order to (un)mute a given player.\n\nThis command will work from any channel, as long as you have Guild Management permissions.\n\nI Also recommend setting up the permissions such that noone can talk in this channel.")
+			}
+			).catch(console.error);
+	});
+})
+
+locateLogs();
+setInterval(locateLogs, 20000);
